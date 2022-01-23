4d69a29328e3bee3fe2f42e80205d168ccfc0c5f
Julian <julian@pop-os.localdomain>
2022-01-23 21:46:59 +0100

    test updated

--- i2c_test.py
+++ i2c_test.py
@@ -3,4 +3,4 @@ import smbus
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x52
 bus = smbus.SMBus(DEVICE_BUS)
-bus.write_byte_data(DEVICE_ADDR, 0x01, 0x00, 0xFF, 0x04)
+bus.write_i2c_block_data(DEVICE_ADDR, 0x01, [0x04])
