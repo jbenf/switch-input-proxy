736e4fcc9f714bc79386c5bec587b74d3a50edbe
Julian Feja <julian@feja.eu>
2022-01-29 14:45:26 +0100

    log removed

--- input-proxy.py
+++ input-proxy.py
@@ -111,7 +111,6 @@ def handleRelativeInput(queue, relQueue, scheduler, resting=False):
     queueIt = False
 
     if relQueue.empty():
-        print('absXY', absXY)
         if not resting:
             resting = True
             queueIt = True
