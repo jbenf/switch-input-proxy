c60d233dfc9c826224f0981af1e295ac0d522095
Julian Feja <julian@feja.eu>
2022-02-07 20:10:51 +0100

    docstring in connection.py fixed

--- lib/connection.py
+++ lib/connection.py
@@ -2,14 +2,16 @@
 
 from smbus2 import SMBus
 
-"""Interface offering to write data into a register"""
+
 class Connection():
-    """write data array into a register"""
+    """Interface offering to write data into a register"""
+    
     def write(self, register: int, data: list):
+        """write data array into a register"""
         pass
 
-"""Class encapsulating a i2c bus device"""
 class I2CConnector(Connection):
+    """Class encapsulating a i2c bus device"""
     def __init__(self, device_bus: int):
         self.device = device_bus
         self.bus = None
@@ -18,16 +20,16 @@ class I2CConnector(Connection):
     def __initialize_bus(self):
         self.bus = SMBus(self.device)
 
-    """write data array into a register of a device"""
     def write(self, addr: int, register: int, data: list):
+        """write data array into a register of a device"""
         try:
             self.bus.write_i2c_block_data(addr, register, data)
         except IOError:
             print('i2c error', addr)
             self.__initialize_bus()
 
-    """send a single byte to a device"""
     def write_byte(self, addr: int, data: int):
+        """send a single byte to a device"""
         try:
             self.bus.write_byte(addr, data)
         except IOError:
@@ -35,8 +37,8 @@ class I2CConnector(Connection):
             self.__initialize_bus()
 
 
-"""Class encapsulating an i2c connection"""
 class I2CConnection(Connection):
+    """Class encapsulating an i2c connection"""
     def __init__(self, address: int, connector: I2CConnector):
         self.address = address
         self.connector = connector
